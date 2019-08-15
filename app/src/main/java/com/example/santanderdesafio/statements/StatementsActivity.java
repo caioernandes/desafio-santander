55d3194afa3abe93b24838cb3eb7f4127bf67cfa
caio.e.nascimento <caio.e.nascimento@accenture.com>
2019-08-15 15:23:25 -0300

    Ajustadas tela de informacoes do usuario e reposicionamento dos elementos da lista

--- app/src/main/java/com/example/santanderdesafio/statements/StatementsActivity.java
+++ app/src/main/java/com/example/santanderdesafio/statements/StatementsActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import com.example.santanderdesafio.BaseActivity;
 import com.example.santanderdesafio.R;
 
+import java.util.Objects;
+
 public class StatementsActivity extends BaseActivity {
 
     public static String TAG = StatementsActivity.class.getSimpleName();
@@ -17,6 +19,9 @@ public class StatementsActivity extends BaseActivity {
     }
 
     private void initComponents() {
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
         addFragment(R.id.frame_container,
                 new StatementsListFragment(),
                 StatementsListFragment.FRAGMENT_TAG);
